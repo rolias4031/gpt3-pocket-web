eed58beda63b1de071ac0bfd7fc3410d7138ccf8
Sailor Metz <sailormetz@gmail.com>
2022-10-03 13:32:08 -0500

    added /api/status

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      Dashboard
+    <div className='my-5 w-full text-center'>
+      <p className=''>Pocket Law App</p>
     </div>
   )
 }
